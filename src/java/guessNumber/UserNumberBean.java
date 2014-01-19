816f2b1b23b413162fab4fe794f176a5a10ea2ed
mjstroh <mstroh@acm.org>
2014-01-19 09:47:27 +0000

    committing now!

---
+++ src/java/guessNumber/UserNumberBean.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package guessNumber;
+
+import java.io.Serializable;
+import java.util.Random;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mstroh
+ */
+@ManagedBean(name="UserNumberBean")
+@SessionScoped
+public class UserNumberBean implements Serializable {
+
+    Integer randomInt;
+    Integer userNumber;
+    String response;
+
+    public String getResponse() {
+       if ((userNumber != null) && (userNumber.compareTo(randomInt) == 0)) {
+
+            //invalidate user session
+            FacesContext context = FacesContext.getCurrentInstance();
+            HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+            session.invalidate();
+
+            return "Yay! You got it!";
+        } else {
+
+            return "<p>Sorry, " + userNumber + " isn't it.</p>"
+                    + "<p>Guess again...</p>";
+        }
+    }
+
+    public Integer getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(Integer userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    /** Creates a new instance of UserNumberBean */
+    public UserNumberBean() {
+        Random randomGR = new Random();
+        randomInt = new Integer(randomGR.nextInt(10));
+        System.out.println("Duke's number: " + randomInt);
+    }
+    
+}
